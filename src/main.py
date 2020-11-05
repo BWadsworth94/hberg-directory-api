66eb3fc2b8d44dadc9da0190690046a93b0a9aaa
Ben Wadsworth <bwadsworth@splashfinancial.com>
2020-11-04 20:31:55 -0500

    Canned examples

--- src/main.py
+++ src/main.py
@@ -1,1 +1,17 @@
-print ("Hello, John!")
+import sys
+
+# TODO: This is bad! Need to figure out how to set deps reliably...
+sys.path.append("C:\\Users\\Ben\\Documents\\Projects\\hberg-directory-api\\deps")
+from flask_api import FlaskAPI
+
+app = FlaskAPI(__name__)
+
+@app.route('/example/<int:key>/<string:name>', methods=['GET'])
+def example(key, name):
+    return {
+        'hello': name,
+        'age': key
+    }
+
+if __name__ == "__main__":
+    app.run(debug=True)
